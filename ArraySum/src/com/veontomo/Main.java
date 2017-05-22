e4b4009c79f16299a9fc6fcb007b35838277e695
Andrey Shcherbakov <veontomo@gmail.com>
2017-05-22 14:48:57 +0200

    Implement Sort

--- ArraySum/src/com/veontomo/Main.java
+++ ArraySum/src/com/veontomo/Main.java
@@ -1,7 +1,6 @@
 package com.veontomo;
 
 
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -12,8 +11,34 @@ public class Main {
         in.nextInt();
         in.nextLine();
         String line = in.nextLine();
-        int sum = Arrays.stream(line.split("\\s+", -1)).mapToInt(Integer::parseInt).sum();
-        System.out.println(sum);
 
+
+        int[] numbers = Arrays.stream(line.split("\\s+", -1)).mapToInt(Integer::parseInt).toArray();
+        insertIntoSorted2(numbers);
+
+    }
+
+    private static void insertIntoSorted2(int[] ar) {
+        for (int i = 1; i < ar.length; i++) {
+            insertIntoSorted(ar, i);
+            printArray(ar);
+        }
+    }
+
+    private static void insertIntoSorted(int[] numbers, int pos) {
+//        int pos = numbers.length - 1;
+        int elem = numbers[pos];
+        while (pos > 0 && numbers[pos - 1] > elem) {
+            numbers[pos] = numbers[pos - 1];
+            pos--;
+        }
+        numbers[pos] = elem;
+    }
+
+    private static void printArray(int[] ar) {
+        for (int n : ar) {
+            System.out.print(n + " ");
+        }
+        System.out.println("");
     }
 }
